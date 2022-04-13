de754545bc62ede73c6fd242fd386323e6bf44c3
Sos0Burger <56436385+Sos0Burger@users.noreply.github.com>
2022-04-13 14:14:11 +0300

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String ip = "Айпи 125.255.1.1";
+        Pattern pattern = Pattern.compile("\\D(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
+        Matcher m = pattern.matcher(ip);
+        if (m.find()){
+            System.out.println(m.group());
+        }
+        else{
+            System.out.println("Нет корректного IP");
+        }
+    }
+}
