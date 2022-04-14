88a2d6da1001541373748b46914d489bc2e428d8
Sos0Burger <56436385+Sos0Burger@users.noreply.github.com>
2022-04-14 18:58:17 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         String ip = "Айпи 125.255.1.1";
-        Pattern pattern = Pattern.compile("\\D(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
+        Pattern pattern = Pattern.compile("\\D|^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
         Matcher m = pattern.matcher(ip);
         if (m.find()){
             System.out.println(m.group());
