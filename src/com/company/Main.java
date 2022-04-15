4a3b9f0c190e3ca287d3156589349520afe64676
Sos0Burger <56436385+Sos0Burger@users.noreply.github.com>
2022-04-15 10:02:16 +0300

    d

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        String ip = "Айпи 125.255.1.1";
-        Pattern pattern = Pattern.compile("\\D(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String ip =in.nextLine();
+        Pattern pattern = Pattern.compile("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
         Matcher m = pattern.matcher(ip);
         if (m.find()){
             System.out.println(m.group());
