0916049fae133b6de2832b62493619e429495f92
Sos0Burger <56436385+Sos0Burger@users.noreply.github.com>
2022-04-23 11:07:53 +0300

    Fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,15 +10,22 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите текст");
         String ip =in.nextLine();
+
+        boolean isFind;
+        int mathcesCount = 0;
         
-        Pattern pattern = Pattern.compile("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
+        Pattern pattern = Pattern.compile("((^|\\s)(([1-9]?\\d|1\\d\\d|2[0-5][0-5]|2[0-4]\\d)\\.){3}([1-9]?\\d|1\\d\\d|2[0-5][0-5]|2[0-4]\\d))");
         Matcher m = pattern.matcher(ip);
         
-        if (m.find()){
-            System.out.println(m.group());
-        }
-        else{
-            System.out.println("Нет корректного IP");
+        while(isFind = m.find()){
+            if(isFind) {
+                System.out.println((m.group()).replace(" ", ""));
+                mathcesCount++;
+            }
+            if(mathcesCount == 0){
+                System.out.println("Нет корректного IP");
+                break;
+            }
         }
     }
 }
